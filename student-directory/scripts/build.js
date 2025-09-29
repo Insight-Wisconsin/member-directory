6855512f404c36b08bb3ed89179cb392f1435368
Test Student <student@example.com>
2025-09-29 18:57:32 -0500

    fixed path error

--- student-directory/scripts/build.js
+++ student-directory/scripts/build.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const studentsDir = path.join(__dirname, "..", "students");
-const outputFile = path.join(__dirname, "..", "students.json");
+const outputFile = path.join(__dirname, "..", "..", "students.json");
 
 const students = [];
 
